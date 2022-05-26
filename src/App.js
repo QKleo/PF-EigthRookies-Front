9cef8801270be8705e35a98e52bfce1c57912c6e
GrossoLuis <luisenriquegrosso@gmail.com>
2022-05-26 13:44:04 -0300

    cambios hechos

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import {Routes, Route} from 'react-router-dom'
 import HomeScreen from './Screens/HomeScreen';
 
-import LoginAuth0 from './Features/Login/LoginAuth0';
+import LoginAuth0 from './features/login/LoginAuth0';
 import NavBar from './components/NavBar/NavBar'
 import ProductDetail from './productDetail';
 
