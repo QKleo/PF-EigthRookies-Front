b58a0fd26f0f06e47de3e94f7c30c5949276e046
Leonardo <leme.310589@gmail.com>
2022-05-26 16:37:50 -0400

    aqui me quede trabado en pedir datos al back

--- src/Components/SearchBox/SearchBox.jsx
+++ src/Components/SearchBox/SearchBox.jsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import style from './SearchBox.module.css';
 
 export default function SearchBox() {
+
+    const navigate = useNavigate();
+
+    const [input, setInput] = useState('');
+
+    const handlerSubmit = (e) => {
+        e.preventDefault();
+        navigate(`/products/buscar?name=${input}`);
+        setInput('');
+    };
+
     return (
-        <div>SearchBox</div>
+        <form onSubmit={handlerSubmit}>
+            <input
+                className={style.input}
+                type="text"
+                autoComplete='on'
+                placeholder='Buscar producto...'
+                onChange={(e) => setInput(e.target.value)}
+                value={input}
+            />
+            <button type="submit">Buscar</button>
+        </form>
     );
 }
