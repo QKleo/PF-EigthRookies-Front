449adbd5429cdfa9c8dd09092f7056ef4f842478
Leonardo <leme.310589@gmail.com>
2022-05-26 00:01:28 -0400

    feat/NavBar funciona con el login

--- src/App.js
+++ src/App.js
@@ -1,28 +1,18 @@
 import { Routes, Route } from 'react-router-dom';
-import './App.css';
 import LoginAuth0 from './Features/Login/LoginAuth0';
-import { useAuth0 } from '@auth0/auth0-react';
+import NavBar from './Components/NavBar/NavBar'
+
 
 function App() {
 
-  const {isAuthenticated, user, logout} = useAuth0()
 
   return (
-    <div className="App">
-      {
-        isAuthenticated
-        ? <div>
-                <img src={user.picture} alt={user.name}/>
-                <h1>Hola {user.name}</h1>
-                <p>{user.email}</p>
-                <button onClick={logout} >Salir</button>
-            </div>
-        : <h1>Bienvenido a "The Rookies"</h1>
-      }
+    <>
+      <NavBar />
       <Routes>
-        <Route path="/login" element={<LoginAuth0 />} />
+        <Route path='/login' element={<LoginAuth0 />} />
       </Routes>
-    </div>
+    </>
   );
 }
 
