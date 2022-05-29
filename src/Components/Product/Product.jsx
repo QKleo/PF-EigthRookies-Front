bbf5aede30d20128cf6f6c9967873d56b69b71e8
GrossoLuis <luisenriquegrosso@gmail.com>
2022-05-29 17:52:49 -0300

    css

--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -1,8 +1,6 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 import s from "../../../src/Global.module.css";
-import a from "./product.module.css";
-import { AiOutlineShoppingCart } from 'react-icons/ai';
-
+import { AiOutlineShoppingCart } from "react-icons/ai";
 
 export default function Product(props) {
   const { products } = props;
@@ -25,24 +23,42 @@ export default function Product(props) {
 
     // </div>
 
-      <div style={{  border:'10px solid #f1f1f1' }}>
-              <div style={{display:'flex',flexDirection:"column"}}>
-                <Link to={`/products/${products.id}`} >
-              <img src={products.image} alt={products.name} width="200px" height="200px" />
-    
-              </Link>
+    <div style={{ border: "2px solid black", borderRadius: "20px" }}>
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div style={{ marginTop: "20px", alignItems:'center'}}>
+          <Link to={`/products/${products.id}`}>
+            <img
+              src={products.image}
+              alt={products.name}
+              width="200px"
+              height="200px"
+            />
+          </Link>
+        </div>
 
-              <Link to={`/products/${products.id}`} >
-    
-              </Link>
-              <h4 >{products.name}</h4>
-                  <button className={s.basicBtn} >Add to <AiOutlineShoppingCart style={{ color: 'white', fontSize: '20px', marginTop:'7px'}} /></button>
-              </div>
+        <Link to={`/products/${products.id}`}>
+          <h4 style={{ textAlign: "center", color: "black" }}>
+            {products.name}
+          </h4>
+        </Link>
 
-
-      </div> 
-
-
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "15px",
+            alignItems: "center",
+          }}
+        >
+          <h4 style={{ fontSize: "20px" }}>${products.price}</h4>
+          <button className={s.btnLand}>
+            <AiOutlineShoppingCart
+              style={{ color: "white", fontSize: "20px", marginTop: "7px" }}
+            />
+          </button>
+        </div>
+      </div>
+    </div>
   );
 }
 //export default Product;
